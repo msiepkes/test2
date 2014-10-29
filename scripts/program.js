4db21e58670b87ee53e976d330078c1f8f92a4b2
msiepkes <martin.siepkes@quicknet.nl>
2014-10-29 21:16:41 +0100

    d

--- scripts/program.js
+++ scripts/program.js
@@ -1,4 +1,4 @@
-var db = window.sqlitePlugin.openDatabase("recheck", "1.0", "recheck", -1);
+var db = window.sqlitePlugin.openDatabase({name: "recheck"});
 var _t = null; 
 var _isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
 var Valid = null;
