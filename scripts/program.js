3ea45a29cbece225ccc1104c70fd0278ee264096
msiepkes <martin.siepkes@quicknet.nl>
2014-10-30 09:01:32 +0100

    d

--- scripts/program.js
+++ scripts/program.js
@@ -13,52 +13,14 @@ var _questionid = null;
 
     // Cordova is ready
     function onDeviceReady() {
-		window.sqlitePlugin.deleteDatabase("DB");
-		
-		
-		
-alert('a1');
-var db = window.sqlitePlugin.openDatabase({name: "DB"});
-alert('a2');
-
-      db.transaction(function(tx) {
-alert('a3');
-        tx.executeSql('DROP TABLE IF EXISTS gebruiker');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS gebruiker (firstname text, lastname text, email text, code text)');
-alert('a4');
- 
-        tx.executeSql("INSERT INTO gebruiker (firstname, lastname, email, code) VALUES (?,?)", ["Martin", "Siepkes", "martin.siepkes@quicknet.nl", "7aafaa3f-ff25-4a5a-91c6-753f30a5a03b"], function(tx, res) {
-          alert("insertId: " + res.insertId + " -- probably 1");
-          alert("rowsAffected: " + res.rowsAffected + " -- should be 1");
-
-          db.transaction(function(tx) {
-            tx.executeSql("select count(*) as cnt from gebruiker;", [], function(tx, res) {
-              alert("res.rows.length: " + res.rows.length + " -- should be 1");
-              alert("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
-            });
-          });
-
-        }, function(e) {
-          alert("ERROR: " + e.message);
-        });
-      });
-	  
-	  
-	  alert('s1');
-  
-	  db.transaction(function(tx) {
-		tx.executeSql("select firstname, lastname from gebruiker;", [], function(tx, res) {
-		  if(res.rows.length > 0) { 
-			alert("voornaam: " + res.rows.item(0).firstname);
-			alert("achternaam: " + res.rows.item(0).lastname);
-		  }
-		});
-	  });
-	  
-	  alert('s1');
+		localStorage.setItem('firstname', 'Martin');
+				
+				   
+	   	
 	  
 	  
 		$(document).on("pageshow","#aanmelden",function(){
+			alert(localStorage.getItem('firstname'));
 			alert('aanmelden');
 		});
     }
